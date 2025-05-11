388c89abaf4ee68c5f4e46c721dfd5fa3d47dbc7
arthurlpsz <arthurlps.santos1@gmail.com>
2025-05-11 17:11:54 -0300

    Estilização da home e sobre nos

---
+++ Downloads/ProjetoDW/app/urls.py
@@ -0,0 +1,12 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.index, name = "app"),
+    path('sobre-nos', views.sobreNos, name="sobreNos"),
+    path('usuarios/', views.exibirUsuarios, name = "exibirUsuarios"),
+    path('add-usuario', views.addUsuario, name="addUsuario"),
+    path('excluir-usuario/<int:id_usuario>', views.excluirUsuario, name="excluirUsuario" ),
+    path('editar-usuario/<int:id_usuario>', views.editarUsuario, name="editarUsuario"),
+    
+]
---
+++ Downloads/ProjetoDW/app/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.template import loader
+from app.models import Usuario
+from app.forms import formUsuario
+
+def exibirUsuarios(request):
+    usuarios = Usuario.objects.all().values()
+    return render(request, "usuarios.html", {'listUsuarios': usuarios})
+
+def index(request):
+    template = loader.get_template("index.html")
+    return HttpResponse(template.render())
+
+def sobreNos(request):
+    template = loader.get_template("sobre-nos.html")
+    return HttpResponse(template.render())
+
+def addUsuario(request):
+    formUser = formUsuario(request.POST or None)
+    if request.POST:
+        if formUser.is_valid():
+            formUser.save()
+            return redirect("exibirUsuarios")
+    return render(request, "add-usuario.html", {'form':formUser})
+
+def excluirUsuario(request, id_usuario):
+    usuario = Usuario.objects.get(id=id_usuario)
+    usuario.delete()
+    return redirect("exibirUsuarios")
+
+def editarUsuario(request, id_usuario):
+    usuario = Usuario.objects.get(id=id_usuario)
+    formUser = formUsuario(request.POST or None, instance=usuario)
+
+    if request.POST:
+        if formUser.is_valid():
+            formUser.save()
+            return redirect("exibirUsuarios")
+    return render(request, "editar-usuario.html", {'form': formUser})
